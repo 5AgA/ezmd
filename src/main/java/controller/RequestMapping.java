1ac5a3fb8e20f4478e14e1233a1bd200386473bc
Lee Yeon <rofttm0033@gmail.com>
2024-11-19 15:03:55 +0900

    [fix] RequestMapping.java 오류나는 부분 주석처리

--- src/main/java/controller/RequestMapping.java
+++ src/main/java/controller/RequestMapping.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestMapping {
-    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
+    /*private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     
     // 각 요청 uri에 대한 controller 객체를 저장할 HashMap 생성
     private Map<String, Controller> mappings = new HashMap<String, Controller>();
@@ -45,5 +45,5 @@ public class RequestMapping {
     public Controller findController(String uri) {	
     	// 주어진 uri에 대응되는 controller 객체를 찾아 반환
         return mappings.get(uri);
-    }
+    }*/
 }
