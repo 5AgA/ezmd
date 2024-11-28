ecf96fae16a67a94055c2fd27610a6b663ac027e
5AgA <silvera03@naver.com>
2024-11-28 20:44:51 +0900

    [ADD] Usr Manager and Controller

--- src/main/java/controller/RequestMapping.java
+++ src/main/java/controller/RequestMapping.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//import controller.user.LoginController;
+//import controller.user.LogoutController;
+//import controller.user.SignupController;
 
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
@@ -15,14 +18,14 @@ public class RequestMapping {
     	// 각 uri에 대응되는 controller 객체를 생성 및 저장
         mappings.put("/", new ForwardController("/index/index.jsp"));
         mappings.put("/login/form", new ForwardController("/user/loginForm.jsp"));
-        mappings.put("/login", new LoginController());
-//        mappings.put("/user/logout", new LogoutController());
+//        mappings.put("/login", new LoginController());
+//        mappings.put("/logout", new LogoutController());
 //        mappings.put("/user/list", new ListUserController());
 //        mappings.put("/user/view", new ViewUserController());
         
         // 회원 가입 폼 요청과 가입 요청 처리 병합 (폼에 커뮤니티 선택 메뉴 추가를 위함)
         mappings.put("/register/form", new ForwardController("/user/registerForm.jsp"));
-        mappings.put("/register", new RegisterUserController());
+//        mappings.put("/register", new SignupController());
 
         // 사용자 정보 수정 폼 요청과 수정 요청 처리 병합
 //      mappings.put("/user/update/form", new UpdateUserFormController());
